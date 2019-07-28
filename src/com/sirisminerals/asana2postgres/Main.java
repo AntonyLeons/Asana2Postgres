f3ca7e1003166e6e54b5e2c81ab274f151db968e
AntonyLeons <antony@leons.dev>
2019-07-28 23:43:24 +0100

    Update Main.java

--- src/com/sirisminerals/asana2postgres/Main.java
+++ src/com/sirisminerals/asana2postgres/Main.java
@@ -8,12 +8,12 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) {
-        String projectid="2760706195514";
+        String project_id="2760706195514";
         String db_user="postgres";
         String db_pass="BodyRail%8";
         String Auth_key="0/1fe378c15de839054e06c60f8e78563f";
         String Api_URL="https://app.asana.com/api/1.0";
-        String Api_req="/projects/2760706195514/tasks?opt_fields=id,assignee,assignee_status,created_at,completed,completed_at,due_on,due_at,external,followers,hearted,hearts,modified_at,name,notes,num_hearts,projects,parent,workspace,memberships&limit=100";
+        String Api_req="/projects/"+project_id+"/tasks?opt_fields=id,assignee,assignee_status,created_at,completed,completed_at,due_on,due_at,external,followers,hearted,hearts,modified_at,name,notes,num_hearts,projects,parent,workspace,memberships&limit=100";
 
         String db_url = "jdbc:postgresql://172.16.132.193:5432/support";
         Properties props = new Properties();
