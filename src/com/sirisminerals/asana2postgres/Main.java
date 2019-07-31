eb659f8362bd6d12b4d194c5d69947c0baad369c
Antony Leons <antony@leons.dev>
2019-07-31 12:25:35 +0100

    fully working import

--- src/com/sirisminerals/asana2postgres/Main.java
+++ src/com/sirisminerals/asana2postgres/Main.java
@@ -1,21 +1,22 @@
 package com.sirisminerals.asana2postgres;
 
+import com.asana.Client;
+import com.asana.models.CustomField;
+import com.asana.models.ResultBodyCollection;
+import com.asana.models.Task;
+import com.asana.models.User;
+import com.asana.requests.CollectionRequest;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
-import java.util.logging.*;
-import java.time.*;
-
-import com.asana.*;
-import com.asana.models.*;
-import com.asana.requests.CollectionRequest;
-import com.google.api.client.util.DateTime;
-import org.apache.commons.lang3.StringUtils;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
@@ -26,6 +27,8 @@ public class Main {
         String db_user = "postgres";
         String db_pass = "BodyRail%8";
         String Auth_key = "0/1fe378c15de839054e06c60f8e78563f";
+        File file = new File("C:\\Users\\aleons\\OneDrive - Sirius Minerals PLC\\Documents\\GitHub\\Asana2Postgres\\data.csv");
+
 
         // Database connection
         String db_url = "jdbc:postgresql://172.16.138.39:5432/support";
@@ -33,30 +36,15 @@ public class Main {
         props.setProperty("user", db_user);
         props.setProperty("password", db_pass);
         props.setProperty("ssl", "false");
+
         try (Connection conn = DriverManager.getConnection(db_url, props)) {
             logger.log(Level.INFO, "Connected " + logger.getName());
             String offset = null;
             final Client client = Client.accessToken(Auth_key);
-            File file = new File("C:\\Users\\aleons\\OneDrive - Sirius Minerals PLC\\Documents\\GitHub\\Asana2Postgres\\data.csv");
             FileWriter writer = new FileWriter(file, false);
-            List<String> fields = new ArrayList<String>(Arrays.asList("id", "created_at", "due_on", "completed", "modified_at", "name", "notes", "assignee", "assignee.name", "assignee.email", "tags", "custom_fields","custom_fields.enum_value"));
-            List<String> expand = new ArrayList<String>(Arrays.asList("id", "created_at", "due_on", "completed", "modified_at", "name", "notes", "assignee", "assignee.name", "assignee.email", "tags", "custom_fields","custom_fields.enum_value"));
+            List<String> fields = new ArrayList<String>(Arrays.asList("id", "created_at", "due_on", "completed", "modified_at", "name", "notes", "assignee", "assignee.name", "assignee.email", "tags", "custom_fields", "custom_fields.enum_value"));
+            List<String> expand = new ArrayList<String>(Arrays.asList("id", "created_at", "due_on", "completed", "modified_at", "name", "notes", "assignee", "assignee.name", "assignee.email", "tags", "custom_fields", "custom_fields.enum_value"));
 
-//            while(true) {
-//                CollectionRequest assignee=client.users.findAll();
-//                ResultBodyCollection<User> userResult=assignee.executeRaw();
-//               for(User i :userResult.data)
-//               {
-//                   i.name,i.email,i.id,i.photo
-//               }
-//                if (userResult.nextPage != null) {
-//                    logger.log(Level.INFO, "Next Page " + logger.getName());
-//                    offset = userResult.nextPage.offset;
-//                } else {
-//                    break;
-//                }
-
-            // }
             while (true) {
                 CollectionRequest tasks = client.tasks.findByProject(project_id).option("limit", 100).option("page_size", 100).option("offset", offset).option("fields", fields).option("expand", expand);
                 CollectionRequest assignee = client.users.findAll();
@@ -64,13 +52,16 @@ public class Main {
                 ResultBodyCollection<Task> result = tasks.executeRaw();
                 for (Task i : result.data) {
                     String assignee_id = "";
+                    String created_at = "";
+                    String completed_at = "";
+                    String modified_at = "";
                     String assignee_name = "";
                     String assignee_email = "";
                     String site = "";
                     String ticket_time = "";
                     String topic = "";
                     String input = "";
-
+                    String due_on = "";
                     if (i.assignee != null) {
                         assignee_id = i.assignee.id;
                         assignee_name = i.assignee.name;
@@ -79,38 +70,35 @@ public class Main {
                     Iterator<CustomField> listIterator = i.customFields.iterator();
                     CustomField a = listIterator.next();
 
-                    if (a != null&&a.enumValue!=null) {
+                    if (a != null && a.enumValue != null) {
                         site = a.enumValue.name;
                     }
                     a = listIterator.next();
-                    if (a != null&&a.enumValue!=null) {
+                    if (a != null && a.enumValue != null) {
                         ticket_time = a.enumValue.name;
                     }
                     a = listIterator.next();
-                    if (a != null&&a.enumValue!=null) {
+                    if (a != null && a.enumValue != null) {
                         topic = a.enumValue.name;
                     }
                     a = listIterator.next();
-                    if (a != null&&a.enumValue!=null) {
+                    if (a != null && a.enumValue != null) {
                         input = a.enumValue.name;
                     }
-                    
-                    //                writer.write(i.id+'~'+i.createdAt+'~'+i.completed+'~'+i.modifiedAt+'~'+i.name+'~'+i.assignee.name+'~'+i.assignee.email+'~'+i.startOn+'~'+i.completedAt+'~'+i.tags+'~'+i.notes+'~'+i.projects+'~'+i.parent+'~'+i.customFields+"\n");
-                    writer.write(i.id + '~' + i.createdAt + '~' + i.completedAt + '~' + i.completed + '~' + i.modifiedAt + '~' + "i.name" + '~' + assignee_name + '~' + assignee_email + '~' + i.dueOn + '~' + "i.notes" + '~' + site + '~' + ticket_time + '~' + topic + '~' + input + "\n");
-//                String sql = "INSERT INTO public.tickets(\"ID\", \"Created\", \"Completed\", \"Modified\", \"Name\", \"Assignee\", \"Assignee_Email\", \"Start_Date\", \"End_Date\", \"Tags\", \"Notes\", \"Projects\", \"Parent_Task\", \"Site\", \"Ticket_Time\", \"Topic\", \"Ticket_Input\", \"Start_DateTime\", \"End_DateTime\") " +
-//                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
-//                String sql = "INSERT INTO public.tickets(\"ID\", \"Created\", \"Completed\", \"Modified\", \"Name\",\"Notes\",\"Start_Date\", \"End_Date\", \"Tags\")"+
-//                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
-//                PreparedStatement ps =conn.prepareStatement(sql);
-//                ps.setString(1,i.id);
-//                ps.setObject(2,i.createdAt);
-//              ps.setBoolean(3,i.completed);
-//                ps.setObject(4,i.modifiedAt);
-//                ps.setString(5,i.name);
-                    // ps.setString(6,i.assignee.email);
+                    if (i.createdAt != null) {
+                        created_at = i.createdAt.toString();
+                    }
+                    if (i.completedAt != null) {
+                        completed_at = i.createdAt.toString();
+                    }
+                    if (i.modifiedAt != null) {
+                        modified_at = i.createdAt.toString();
+                    }
+                    if (i.modifiedAt != null) {
+                        due_on = i.modifiedAt.toString();
+                    }
+                    writer.write(i.id + '~' + created_at + '~' + completed_at + '~' + i.completed + '~' + modified_at + '~' + i.name + '~' + assignee_name + '~' + assignee_email + '~' + due_on + '~' + i.notes + '~' + site + '~' + ticket_time + '~' + topic + '~' + input + "\n");
 
-
-                    // System.out.println(ps.execute(sql));
                 }
                 if (result.nextPage != null) {
                     logger.log(Level.INFO, "Next Page " + logger.getName());
@@ -120,31 +108,15 @@ public class Main {
                 }
 
             }
-writer.close();
+            writer.close();
+            Statement statement = conn.createStatement();
+            String sql = "COPY tickets FROM \'/media/sf_Asana2Postgres/data.csv\' DELIMITERS \'~\' CSV";
 
+            System.out.println(statement.executeUpdate(sql));
         } catch (SQLException e) {
             logger.log(Level.SEVERE, "Failed to connect to db" + logger.getName());
             e.printStackTrace();
         }
 
     }
-//    public static Task Cleaninputs(Task l)
-//    {
-//        if(l.name==null)
-//            l.name = "";
-//        if(l.createdAt==null)
-//            l.createdAt = DateTime.parseRfc3339("1999-01-01T12:00:00-00:00");
-//
-//        if(l.modifiedAt==null)
-//            l.modifiedAt = DateTime.parseRfc3339("1999-01-01T12:00:00-00:00");
-//        if(l.dueOn==null)
-//            l.dueOn = DateTime.parseRfc3339("1999-01-01T12:00:00-00:00");
-//        if(l.name==null)
-//            l.name="";
-//        if(l.tags==null)
-//        {
-//            l.tags=Collections.EMPTY_LIST;
-//        }
-//        return l;
-//    }
 }
