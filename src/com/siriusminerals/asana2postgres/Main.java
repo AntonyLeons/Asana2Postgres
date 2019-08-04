2dd48641ba4953801d3c084357246a116cf82e65
AntonyLeons <antony@leons.dev>
2019-08-04 11:55:28 +0100

    uncomment most code

--- src/com/siriusminerals/asana2postgres/Main.java
+++ src/com/siriusminerals/asana2postgres/Main.java
@@ -20,7 +20,7 @@ public class Main {
         final Logger logger = Logger.getLogger(Main.class.getName());
         //config
         String project_id = "2760706195514";
-        String db_user = "postgres";
+        String db_user = System.getenv("db_user");
         String db_pass = System.getenv("db_pass");
         String Auth_key = System.getenv("TOKEN");
         String CSV_Filepath = System.getenv("Filepath");
@@ -44,71 +44,71 @@ public class Main {
         List<String> fields2 = new ArrayList<>(Arrays.asList("id", "name", "notes"));
         List<String> expand2 = new ArrayList<>(Arrays.asList("id", "name", "notes"));
 
-//        File file = new File(CSV_Filepath);
-//        FileWriter writer = new FileWriter(file, false);
-//
-//        while (true) {
-//            CollectionRequest tasks = client.tasks.findByProject(project_id).option("limit", 100).option("page_size", 100).option("offset", offset).option("fields", fields).option("expand", expand);
-//            ResultBodyCollection<Task> result = tasks.executeRaw();
-//            for (Task i : result.data) {
-//                String assignee_id = "";
-//                String created_at = "";
-//                String completed_at = "";
-//                String modified_at = "";
-//                String due_on = "";
-//                String assignee_name = "";
-//                String assignee_email = "";
-//                String site = "";
-//                String ticket_time = "";
-//                String topic = "";
-//                String input = "";
-//
-//                if (i.assignee != null) {
-//                    assignee_id = i.assignee.id;
-//                    assignee_name = i.assignee.name;
-//                    assignee_email = i.assignee.email;
-//                }
-//                Iterator<CustomField> listIterator = i.customFields.iterator();
-//                CustomField a = listIterator.next();
-//
-//                if (a != null && a.enumValue != null) {
-//                    site = a.enumValue.name;
-//                }
-//                a = listIterator.next();
-//                if (a != null && a.enumValue != null) {
-//                    ticket_time = a.enumValue.name;
-//                }
-//                a = listIterator.next();
-//                if (a != null && a.enumValue != null) {
-//                    topic = a.enumValue.name;
-//                }
-//                a = listIterator.next();
-//                if (a != null && a.enumValue != null) {
-//                    input = a.enumValue.name;
-//                }
-//                if (i.createdAt != null) {
-//                    created_at = String.valueOf(i.createdAt);
-//                }
-//                if (i.completedAt != null) {
-//                    completed_at = String.valueOf(i.createdAt);
-//                }
-//                if (i.modifiedAt != null) {
-//                    modified_at = String.valueOf(i.createdAt);
-//                }
-//                if (i.dueOn != null) {
-//                    due_on = i.dueOn.toString();
-//                }
-//                writer.write(i.id + '~' + created_at + '~' + completed_at + '~' + i.completed + '~' + modified_at + '~' + "name" + '~' + assignee_name + '~' + assignee_email + '~' + due_on + '~' + "notes" + '~' + site + '~' + ticket_time + '~' + topic + '~' + input + "\n");
-//            }
-//            if (result.nextPage != null) {
-//                logger.log(Level.INFO, "Next Page " + logger.getName());
-//                offset = result.nextPage.offset;
-//            } else {
-//                break;
-//            }
-//
-//        }
-//        writer.close();
+        File file = new File(CSV_Filepath);
+        FileWriter writer = new FileWriter(file, false);
+
+        while (true) {
+            CollectionRequest tasks = client.tasks.findByProject(project_id).option("limit", 100).option("page_size", 100).option("offset", offset).option("fields", fields).option("expand", expand);
+            ResultBodyCollection<Task> result = tasks.executeRaw();
+            for (Task i : result.data) {
+                String assignee_id = "";
+                String created_at = "";
+                String completed_at = "";
+                String modified_at = "";
+                String due_on = "";
+                String assignee_name = "";
+                String assignee_email = "";
+                String site = "";
+                String ticket_time = "";
+                String topic = "";
+                String input = "";
+
+                if (i.assignee != null) {
+                    assignee_id = i.assignee.id;
+                    assignee_name = i.assignee.name;
+                    assignee_email = i.assignee.email;
+                }
+                Iterator<CustomField> listIterator = i.customFields.iterator();
+                CustomField a = listIterator.next();
+
+                if (a != null && a.enumValue != null) {
+                    site = a.enumValue.name;
+                }
+                a = listIterator.next();
+                if (a != null && a.enumValue != null) {
+                    ticket_time = a.enumValue.name;
+                }
+                a = listIterator.next();
+                if (a != null && a.enumValue != null) {
+                    topic = a.enumValue.name;
+                }
+                a = listIterator.next();
+                if (a != null && a.enumValue != null) {
+                    input = a.enumValue.name;
+                }
+                if (i.createdAt != null) {
+                    created_at = String.valueOf(i.createdAt);
+                }
+                if (i.completedAt != null) {
+                    completed_at = String.valueOf(i.createdAt);
+                }
+                if (i.modifiedAt != null) {
+                    modified_at = String.valueOf(i.createdAt);
+                }
+                if (i.dueOn != null) {
+                    due_on = i.dueOn.toString();
+                }
+                writer.write(i.id + '~' + created_at + '~' + completed_at + '~' + i.completed + '~' + modified_at + '~' + "name" + '~' + assignee_name + '~' + assignee_email + '~' + due_on + '~' + "notes" + '~' + site + '~' + ticket_time + '~' + topic + '~' + input + "\n");
+            }
+            if (result.nextPage != null) {
+                logger.log(Level.INFO, "Next Page " + logger.getName());
+                offset = result.nextPage.offset;
+            } else {
+                break;
+            }
+
+        }
+        writer.close();
         try (Connection conn = DriverManager.getConnection(db_url, props)) {
             Statement delete = conn.createStatement();
             String deleteSQL = "TRUNCATE tickets";
